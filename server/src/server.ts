3fab64fa6f7708bcfdc6daadc6e8cc17f8000d12
ryannicholas <ryannicholas.vieira@gmail.com>
2023-05-21 16:44:43 -0300

    feat: :sparkles: add static archive upload

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,12 +3,21 @@ import 'dotenv/config'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
 import { memoriesRoutes } from './routes/memories'
+import { uploadRoutes } from './routes/upload'
 import { authRoutes } from './routes/auth'
+import { resolve } from 'path'
 
 const app = fastify()
 
 const port: number = Number(process.env.PORT) || 5000
+
+app.register(multipart)
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads/'),
+  prefix: '/uploads',
+})
 
 app.register(cors, {
   origin: true,
@@ -20,6 +29,7 @@ app.register(jwt, {
 
 app.register(authRoutes)
 app.register(memoriesRoutes)
+app.register(uploadRoutes)
 
 app
   .listen({
