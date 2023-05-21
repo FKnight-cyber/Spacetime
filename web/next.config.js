15cf3d05ac2251242163371a30f1bde3f0749419
ryannicholas <ryannicholas.vieira@gmail.com>
2023-05-21 20:33:39 -0300

    fix: :bug: fix emptymemory component, remove db and build files, add comment on next.config

--- web/next.config.js
+++ web/next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Add your host
   images: {
-    domains: ['avatars.githubusercontent.com'],
+    domains: ['avatars.githubusercontent.com', '192.168.1.74'],
   },
 }
 
